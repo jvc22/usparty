816b24e129eba3be3a714b4053a3814f5fdf57f7
joaovcostas <jvc22102003@gmail.com>
2023-09-19 17:33:42 -0300

    added user search modal

---
+++ src/pages/api/user/index.ts
@@ -0,0 +1,26 @@
+import { prisma } from '@/lib/prisma'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if(req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const { username } = req.body
+
+  if(!username) {
+    return res.status(404).json({message: 'undefined'})
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username
+    }
+  })
+
+  if(!user) {
+    return res.status(404).json({message: `Usuário não encontrado.`})
+  }
+
+  return res.status(200).json({message: `Usuário encontrado.`})
+}
